3ab5f343b4f6d8564bff6a1c07f0744e38f29e2f
claire <clairebontempo@gmail.com>
2020-11-02 12:48:07 -0800

    commit tree 397c94dd53c77ea1c5b855763a619dce9d91bf89

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+
+    attr_accessor :roster, :add_student, :grade, :name
+    
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []  #pipes are or/equals? or wuuuut
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        alpha_roster = {} 
+        @roster.collect do |grade, name|
+            alpha_roster[grade] = name.sort
+        end
+        alpha_roster
+    end
+end
